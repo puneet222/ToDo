79b1ac306028b2a5aa6f256b39451e1530cbcc2a
puneet aggarwal <aggarwal.puneet222@gmail.com>
2019-11-15 13:44:38 +0530

    setup fixed

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,19 @@
-import { combineReducers } from "redux";
-import appReducer from "./appReducer";
+// import { combineReducers } from "redux";
+// import appReducer from "./appReducer";
 
-export default combineReducers({
-  app: appReducer
-});
+// export default combineReducers({
+//   app: appReducer
+// });
+
+import { CREATE_BUCKET } from "../actions/types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case CREATE_BUCKET: {
+      console.log(action);
+      return state;
+    }
+    default:
+      return state;
+  }
+};
